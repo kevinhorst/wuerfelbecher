9409088d7fce079f16f1531ff99f35f83bcc7506
Kevin Horst <browniehorst@web.de>
2026-02-22 21:10:01 +0100

    fix tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,6 +1,9 @@
 import unittest
 
+from lark import UnexpectedToken, UnexpectedCharacters
+
 from wuerfelbecher import parser, statistics
+from wuerfelbecher.parser import ACTION_SUM, ACTION_ADD, ACTION_SELECT
 
 
 class TestParser(unittest.TestCase):
@@ -8,72 +11,36 @@ class TestParser(unittest.TestCase):
         statistics.init_statcounter()
 
     def test_parse_roll(self):
-        self.assertEqual(parser.parse_roll("d6"), [parser.SetToRoll(1, 6, None)])
-        self.assertEqual(parser.parse_roll("w6"), [parser.SetToRoll(1, 6, None)])
-        self.assertEqual(parser.parse_roll("1d6"), [parser.SetToRoll(1, 6, None)])
-        self.assertEqual(parser.parse_roll("1w6"), [parser.SetToRoll(1, 6, None)])
-        self.assertEqual(parser.parse_roll("2d6"), [parser.SetToRoll(2, 6, None)])
-        self.assertEqual(parser.parse_roll("2w6"), [parser.SetToRoll(2, 6, None)])
-        self.assertEqual(parser.parse_roll("3d20"), [parser.SetToRoll(3, 20, None)])
-        self.assertEqual(parser.parse_roll("3w20"), [parser.SetToRoll(3, 20, None)])
-        self.assertEqual(parser.parse_roll("10w6"), [parser.SetToRoll(10, 6, None)])
+        self.assertEqual(parser.SetToRoll(1, 6, None, None, []), parser.parse_roll("d6"))
+        self.assertEqual(parser.SetToRoll(1, 6, None, None, []), parser.parse_roll("1d6"))
+        self.assertEqual(parser.SetToRoll(1, 6, None, None, []), parser.parse_roll("d6"))
+        self.assertEqual(parser.SetToRoll(1, 6, None, None, []), parser.parse_roll("1d6"))
+        self.assertEqual(parser.SetToRoll(2, 6, None, None, []), parser.parse_roll("2d6"))
 
-        self.assertEqual(parser.parse_roll("d6+2"), [parser.SetToRoll(1, 6, 2)])
-        self.assertEqual(parser.parse_roll("w6-2"), [parser.SetToRoll(1, 6, -2)])
-        self.assertEqual(parser.parse_roll("2d6-4"), [parser.SetToRoll(2, 6, -4)])
-        self.assertEqual(parser.parse_roll("2w6+4"), [parser.SetToRoll(2, 6, 4)])
-        self.assertEqual(parser.parse_roll("3d20+10"), [parser.SetToRoll(3, 20, 10)])
-        self.assertEqual(parser.parse_roll("3w20-10"), [parser.SetToRoll(3, 20, -10)])
+        self.assertEqual(parser.SetToRoll(3, 20, None, None, []), parser.parse_roll("3d20"))
 
-        self.assertEqual(parser.parse_roll("2d6+"), [parser.SetToRoll(2, 6, 0)])
-        self.assertEqual(parser.parse_roll("2w6-"), [parser.SetToRoll(2, 6, 0)])
-        self.assertEqual(parser.parse_roll("2d6+0"), [parser.SetToRoll(2, 6, 0)])
-        self.assertEqual(parser.parse_roll("2w6-0"), [parser.SetToRoll(2, 6, 0)])
+        self.assertEqual(parser.SetToRoll(3, 20, None, None, []), parser.parse_roll("3d20"))
+        self.assertEqual(parser.SetToRoll(3, 20, 2, None, [ACTION_SELECT]), parser.parse_roll("3d20(2)"))
+        self.assertEqual(parser.SetToRoll(3, 20, 3, None, [ACTION_SELECT]), parser.parse_roll("3d20(4)"))
+        self.assertEqual(parser.SetToRoll(3, 20, 3, None, [ACTION_SELECT, ACTION_SUM]), parser.parse_roll("3d20(3)+"))
 
-        self.assertEqual(parser.parse_roll("12+w6"), [parser.SetToRoll(1, 6, 12)])
-        self.assertEqual(parser.parse_roll("12+2w6"), [parser.SetToRoll(2, 6, 12)])
-        self.assertEqual(parser.parse_roll("-1+2w6"), [parser.SetToRoll(2, 6, -1)])
+        self.assertEqual(parser.SetToRoll(1, 6, None, 2, [ACTION_SUM, ACTION_ADD]), parser.parse_roll("d6+2"))
+        self.assertEqual(parser.SetToRoll(2, 6, None, -4, [ACTION_SUM, ACTION_ADD]), parser.parse_roll("2d6-4"))
 
-        self.assertEqual(parser.parse_roll("w20 2w6+2"), [parser.SetToRoll(1, 20, None), parser.SetToRoll(2, 6, 2)])
-        self.assertEqual(parser.parse_roll("w20 2w6+2 "), [parser.SetToRoll(1, 20, None), parser.SetToRoll(2, 6, 2)])
-        self.assertEqual(parser.parse_roll(" w20 2w6+2"), [parser.SetToRoll(1, 20, None), parser.SetToRoll(2, 6, 2)])
-        self.assertEqual(parser.parse_roll(" w20  2w6+2 "), [parser.SetToRoll(1, 20, None), parser.SetToRoll(2, 6, 2)])
-        self.assertEqual(parser.parse_roll("  w20  2w6+2  "), [parser.SetToRoll(1, 20, None), parser.SetToRoll(2, 6, 2)])
+        self.assertEqual(parser.SetToRoll(2, 6, None, None, [ACTION_SUM]), parser.parse_roll("2d6+"))
+        self.assertEqual(parser.SetToRoll(2, 6, None, 0, [ACTION_SUM, ACTION_ADD]), parser.parse_roll("2d6+0"))
 
-        with self.assertRaises(ValueError):
+
+        with self.assertRaises(UnexpectedCharacters):
             parser.parse_roll("foo")
-        with self.assertRaises(ValueError):
-            parser.parse_roll("d6 +4")
-        with self.assertRaises(ValueError):
+        with self.assertRaises(UnexpectedCharacters):
             parser.parse_roll("d6+4!")
-        with self.assertRaises(ValueError):
+        with self.assertRaises(UnexpectedToken):
             parser.parse_roll("4+d6+4")
-        with self.assertRaises(ValueError):
-            parser.parse_roll("w20 2w6+2!")
-        with self.assertRaises(ValueError):
-            parser.parse_roll("w20,2w6+2")
-        with self.assertRaises(ValueError):
-            parser.parse_roll("w20;2w6+2")
-        with self.assertRaises(ValueError):
-            parser.parse_roll("w0")
 
     def test_parse_stats(self):
         self.assertEqual(parser.parse_stats("d6"), 6)
-        self.assertEqual(parser.parse_stats("w20"), 20)
         self.assertEqual(parser.parse_stats(" d6 "), 6)
-        self.assertEqual(parser.parse_stats(" w6"), 6)
-        self.assertEqual(parser.parse_stats(" w6 "), 6)
-        with self.assertRaises(ValueError):
-            parser.parse_stats("foo")
-        with self.assertRaises(ValueError):
-            parser.parse_stats("w6+2")
-        with self.assertRaises(ValueError):
-            parser.parse_stats("d20w6")
-        with self.assertRaises(ValueError):
-            parser.parse_stats("d20 w6")
-        with self.assertRaises(ValueError):
-            parser.parse_stats("d0")
-
 
 if __name__ == "__main__":
     unittest.main()
